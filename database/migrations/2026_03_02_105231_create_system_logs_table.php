5309858cd54a5066100c5f161abf7efb7bf6772e
nana72848e <d1041211005@student.untan.ac.id>
2026-03-02 10:55:52 +0700

    implement reset point automatic when intern check in

---
+++ database/migrations/2026_03_02_105231_create_system_logs_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('system_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('action_name')->unique(); // Contoh: 'reset_poin_2023_10'
+            $table->timestamp('executed_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('system_logs');
+    }
+};
