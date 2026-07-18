6808edae7c0ab6e200be30f3a98b8f3ed625e311
Abimanyu Ridho <abimanyu6r@gmail.com>
2026-07-18 14:11:08 +0700

    Update intern

---
+++ database/seeders/Batch2Seeder.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Intern;
+use App\Models\Division;
+
+class Batch2Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // 1. Ambil ID Divisi yang sudah ada di database
+        $divSoftwareDev = Division::where('nama_divisi', 'Software Development')->first();
+        $divSocMed = Division::where('nama_divisi', 'Social Media Specialist')->first();
+        $divDesign = Division::where('nama_divisi', 'Design and Video Editing')->first();
+        $divDataManage = Division::where('nama_divisi', 'Data Management')->first();
+
+        // 2. Data karyawan Batch 2 yang BENAR-BENAR BARU
+        // Karyawan lama yang lanjut ke Batch 2 tidak di-insert lagi karena menggunakan data lama
+        $newInterns = [
+            [
+                'name' => 'Agung Sulaiman Al Hajji',
+                'division_id' => $divDataManage->id,
+                'foto' => 'batch2/Agung Sulaiman Al Hajji.png',
+                'senin' => true, 'selasa' => true, 'rabu' => true, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Arni Nazira',
+                'division_id' => $divDesign->id,
+                'foto' => 'batch2/Arni Nazira.png',
+                'senin' => false, 'selasa' => false, 'rabu' => false, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Aslam Fadholi Safkha',
+                'division_id' => $divSoftwareDev->id,
+                'foto' => 'batch2/Aslam Fadholi Safkha.png',
+                'senin' => false, 'selasa' => true, 'rabu' => false, 'kamis' => true, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Ava Novalina Zahrani',
+                'division_id' => $divDesign->id,
+                'foto' => 'batch2/Ava Novalina Zahrani.png',
+                'senin' => false, 'selasa' => true, 'rabu' => false, 'kamis' => true, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Balin Niswah',
+                'division_id' => $divDesign->id,
+                'foto' => 'batch2/Balin Niswah.png',
+                'senin' => true, 'selasa' => false, 'rabu' => true, 'kamis' => false, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Cindy Fatika Sari',
+                'division_id' => $divDesign->id,
+                'foto' => 'batch2/Cindy Fatika Sari.png',
+                'senin' => true, 'selasa' => true, 'rabu' => false, 'kamis' => false, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Dewi Hasimah',
+                'division_id' => $divDataManage->id,
+                'foto' => 'batch2/Dewi Hasimah.png',
+                'senin' => true, 'selasa' => true, 'rabu' => true, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Dodi Permana',
+                'division_id' => $divDataManage->id,
+                'foto' => 'batch2/Dodi Permana.png',
+                'senin' => true, 'selasa' => true, 'rabu' => true, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Erlys Indriawati Br Tarigan',
+                'division_id' => $divSocMed->id,
+                'foto' => 'batch2/Erlys Indriawati Br Tarigan.png',
+                'senin' => true, 'selasa' => false, 'rabu' => true, 'kamis' => false, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Fahrizsa Anggie Arifia',
+                'division_id' => $divSocMed->id,
+                'foto' => 'batch2/Fahrizsa Anggie Arifia.png',
+                'senin' => true, 'selasa' => false, 'rabu' => false, 'kamis' => true, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Gian Aryanta Putra',
+                'division_id' => $divSoftwareDev->id,
+                'foto' => 'batch2/Gian Aryanta Putra.png',
+                'senin' => true, 'selasa' => false, 'rabu' => false, 'kamis' => true, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Ikbal Sawaludin',
+                'division_id' => $divSoftwareDev->id,
+                'foto' => 'batch2/Ikbal Sawaludin.png',
+                'senin' => true, 'selasa' => false, 'rabu' => false, 'kamis' => true, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Jackland Avanza Siringoringo',
+                'division_id' => $divDataManage->id,
+                'foto' => 'batch2/Jackland Avanza Siringoringo.png',
+                'senin' => true, 'selasa' => true, 'rabu' => true, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Muhammad Darin Noah',
+                'division_id' => $divDesign->id,
+                'foto' => 'batch2/Muhammad Darin Noah.png',
+                'senin' => true, 'selasa' => false, 'rabu' => true, 'kamis' => false, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Muhammad Zulkifli',
+                'division_id' => $divSoftwareDev->id,
+                'foto' => 'batch2/Muhammad Zulkifli.png',
+                'senin' => false, 'selasa' => true, 'rabu' => false, 'kamis' => true, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Mujahidah Mutiara R',
+                'division_id' => $divSocMed->id,
+                'foto' => 'batch2/Mujahidah Mutiara R.png',
+                'senin' => false, 'selasa' => true, 'rabu' => false, 'kamis' => true, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Naaila Maysaqilah',
+                'division_id' => $divDesign->id,
+                'foto' => 'batch2/Naaila Maysaqilah.png',
+                'senin' => false, 'selasa' => false, 'rabu' => false, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Nabila Indaswari',
+                'division_id' => $divSoftwareDev->id,
+                'foto' => 'batch2/Nabila Indaswari.png',
+                'senin' => false, 'selasa' => true, 'rabu' => false, 'kamis' => false, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Nadya Aulia Putri Azzahra',
+                'division_id' => $divDesign->id,
+                'foto' => 'batch2/Nadya Aulia Putri Azzahra.png',
+                'senin' => false, 'selasa' => true, 'rabu' => false, 'kamis' => true, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Oktria Lulu Kataru',
+                'division_id' => $divDataManage->id,
+                'foto' => 'batch2/Oktria Lulu Kataru.png',
+                'senin' => true, 'selasa' => true, 'rabu' => true, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Rafli Pratama',
+                'division_id' => $divSoftwareDev->id,
+                'foto' => 'batch2/Rafli Pratama.png',
+                'senin' => false, 'selasa' => false, 'rabu' => true, 'kamis' => false, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Rara Amiyani',
+                'division_id' => $divSocMed->id,
+                'foto' => 'batch2/Rara Amiyani.png',
+                'senin' => false, 'selasa' => true, 'rabu' => false, 'kamis' => false, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Rayhan Nuerjamman',
+                'division_id' => $divSoftwareDev->id,
+                'foto' => 'batch2/Rayhan Nuerjamman.png',
+                'senin' => true, 'selasa' => false, 'rabu' => true, 'kamis' => false, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Reisya Nayla',
+                'division_id' => $divDataManage->id,
+                'foto' => 'batch2/Reisya Nayla.png',
+                'senin' => true, 'selasa' => true, 'rabu' => true, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Sandhika Julian Nugraha',
+                'division_id' => $divDataManage->id,
+                'foto' => 'batch2/Sandhika Julian Nugraha.png',
+                'senin' => true, 'selasa' => true, 'rabu' => true, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Siti Aisyah Amalia',
+                'division_id' => $divDataManage->id,
+                'foto' => 'batch2/Siti Aisyah Amalia.png',
+                'senin' => true, 'selasa' => true, 'rabu' => true, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Siti Muamalah',
+                'division_id' => $divSocMed->id,
+                'foto' => 'batch2/Siti Muamalah.png',
+                'senin' => false, 'selasa' => false, 'rabu' => true, 'kamis' => true, 'jumat' => false,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Tiara Aulia',
+                'division_id' => $divDataManage->id,
+                'foto' => 'batch2/Tiara Aulia.png',
+                'senin' => true, 'selasa' => true, 'rabu' => true, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Titania Putri',
+                'division_id' => $divDataManage->id,
+                'foto' => 'batch2/Titania Putri.png',
+                'senin' => true, 'selasa' => true, 'rabu' => true, 'kamis' => true, 'jumat' => true,
+                'poin' => 5,
+            ],
+            [
+                'name' => 'Wanda Maulidya',
+                'division_id' => $divSocMed->id,
+                'foto' => 'batch2/Wanda Maulidya.png',
+                'senin' => true, 'selasa' => true, 'rabu' => false, 'kamis' => false, 'jumat' => true,
+                'poin' => 5,
+            ],
+        ];
+
+        // 3. Update foto untuk karyawan lama yang lanjut ke Batch 2
+        $oldInterns = [
+            'Abimanyu Ridho Ramadhani',
+            'Berlian Auraly Kastyanos',
+            'Fandri Gea',
+            'Lusia Odiliana Menge',
+            'Melisa Vikayana',
+            'Syariffullah'
+        ];
+
+        foreach ($oldInterns as $name) {
+            Intern::where('name', $name)->update(['foto' => 'batch2/' . $name . '.png']);
+        }
+
+        foreach ($newInterns as $intern) {
+            // Menggunakan firstOrCreate agar tidak duplikat jika seeder tidak sengaja dijalankan 2x
+            Intern::firstOrCreate(
+                ['name' => $intern['name']], 
+                $intern
+            );
+        }
+    }
+}
